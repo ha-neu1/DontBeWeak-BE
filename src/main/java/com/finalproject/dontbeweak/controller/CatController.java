d7fc7519c3f2b69e1fee76426cece55dc0663bfc
HRYUN <yunhyerim41@gmail.com>
2022-08-12 00:32:46 +0900

    [Cat] - 친구 고양이 조회 (완료)

--- src/main/java/com/finalproject/dontbeweak/controller/CatController.java
+++ src/main/java/com/finalproject/dontbeweak/controller/CatController.java
@@ -1,6 +1,7 @@
 package com.finalproject.dontbeweak.controller;
 
 import com.finalproject.dontbeweak.dto.CatResponseDto;
+import com.finalproject.dontbeweak.model.Cat;
 import com.finalproject.dontbeweak.service.CatService;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -8,10 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
@@ -23,18 +21,25 @@ public class CatController {
 
 
     // 내 고양이 조회
-
     @GetMapping("/cat")
-    public ResponseEntity<CatResponseDto> getMyCatStatus(@AuthenticationPrincipal UserDetails userDetails) {
+    public ResponseEntity<CatResponseDto> getMyCatStatus
+    (@AuthenticationPrincipal UserDetails userDetails) {
+
         CatResponseDto catResponseDto = catService.getMyCatStatus(userDetails);
 
         return ResponseEntity.status(HttpStatus.OK)
                 .body(catResponseDto);
     }
 
-    // 고양이 경험치 상승
-
-    // 고양이 LEVEL UP
 
     // 친구 고양이 조회
+    @GetMapping("/cat/{username}")
+    public ResponseEntity<CatResponseDto> getFriendCatStatus
+    (@PathVariable("username") String username) {
+
+        CatResponseDto friendCatResponseDto = catService.getFriendCatStatus(username);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(friendCatResponseDto);
+    }
 }
