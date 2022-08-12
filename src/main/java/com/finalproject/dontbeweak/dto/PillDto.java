744b351f7a0ab4c3ac2880f41876b3c9fcc382fc
ha-neu1 <107821879+ha-neu1@users.noreply.github.com>
2022-08-13 02:21:38 +0900

    pilldto 수정

---
+++ src/main/java/com/finalproject/dontbeweak/dto/PillDto.java
@@ -0,0 +1,11 @@
+package com.finalproject.dontbeweak.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PillDto {
+    private String productName;
+    private String customColor;
+}
