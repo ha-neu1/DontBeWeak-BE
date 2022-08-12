503c47d28afdfe192b16ecd0ef3d218e07aa3a99
ha-neu1 <107821879+ha-neu1@users.noreply.github.com>
2022-08-13 02:31:45 +0900

    PillController 수정

--- src/main/java/com/finalproject/dontbeweak/controller/PillController.java
+++ src/main/java/com/finalproject/dontbeweak/controller/PillController.java
@@ -1,13 +1,17 @@
 package com.finalproject.dontbeweak.controller;
 
-import com.finalproject.dontbeweak.dto.PillRequestDto;
+import com.finalproject.dontbeweak.dto.PillDto;
 import com.finalproject.dontbeweak.model.Pill;
-import com.finalproject.dontbeweak.model.User;
 import com.finalproject.dontbeweak.repository.PillRepository;
+import com.finalproject.dontbeweak.security.UserDetailsImpl;
 import com.finalproject.dontbeweak.service.PillService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -19,15 +23,15 @@ public class PillController {
 
     //영양제 등록
     @PostMapping("/schedule")
-    public ResponseEntity<Pill> registerPill(@PathVariable User username, @RequestBody List<PillRequestDto> pillRequestDto) {
-        pillService.registerPill(username, pillRequestDto);
-        return ResponseEntity.ok().body();
+    public ResponseEntity<Pill> createPill(@RequestBody List<PillDto> pillDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        pillService.registerPill(pillDto, userDetails);
+        return ResponseEntity.ok().body(null);
     }
 
     //영양제 조회
     @GetMapping("/schedule")
-    public ResponseEntity<List<Pill>> getPill(@PathVariable User username) {
-        List<Pill> pillList = pillRepository.findAllByPill(username);
+    public ResponseEntity<List<PillDto>> getPill(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        List<PillDto> pillList = pillService.showPill(userDetails);
         return ResponseEntity.ok().body(pillList);
     }
 }
