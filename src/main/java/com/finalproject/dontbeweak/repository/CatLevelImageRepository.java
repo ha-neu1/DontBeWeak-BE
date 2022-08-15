67c926d9199c7fbfe1003913f298678d69a08e2c
ha-neu1 <107821879+ha-neu1@users.noreply.github.com>
2022-08-15 19:27:37 +0900

    [cat] -고양이 레벨 사진 레포지토리 삭제

--- src/main/java/com/finalproject/dontbeweak/repository/CatLevelImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.finalproject.dontbeweak.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface CatLevelImageRepository extends JpaRepository<CatLevelImage, Long>{
-    Optional<CatLevelImage> findByLevel(int level);
-}
