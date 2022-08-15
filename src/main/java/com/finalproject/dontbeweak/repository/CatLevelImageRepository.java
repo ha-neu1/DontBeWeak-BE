8a5e7eba226fb9ab27bd0d6954ac082e0b90a54f
ha-neu1 <107821879+ha-neu1@users.noreply.github.com>
2022-08-15 16:30:09 +0900

    합치는 중입니다

--- src/main/java/com/finalproject/dontbeweak/repository/CatLevelImageRepository.java
+++ src/main/java/com/finalproject/dontbeweak/repository/CatLevelImageRepository.java
@@ -1,6 +1,5 @@
 package com.finalproject.dontbeweak.repository;
 
-import com.finalproject.dontbeweak.model.CatLevelImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
