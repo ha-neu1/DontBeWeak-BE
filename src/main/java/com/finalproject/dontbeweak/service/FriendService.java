1a61f50dabed8f0c8c21d72e769ac522a39ab2c4
ju0song <cuobalee@naver.com>
2022-08-16 18:47:13 +0900

    [friend] -자기 자신을 추가 못하게 함 -이미 등록된 친구를 친구 추가 할 수 없도록 함 -주석 정리

--- src/main/java/com/finalproject/dontbeweak/service/FriendService.java
+++ src/main/java/com/finalproject/dontbeweak/service/FriendService.java
@@ -16,6 +16,7 @@ import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.finalproject.dontbeweak.exception.ErrorCode.FRIEND_ADD_CODE;
 import static com.finalproject.dontbeweak.exception.ErrorCode.FRIEND_CHECK_CODE;
 
 
@@ -29,13 +30,20 @@ public class FriendService {
     //친구추가
     @Transactional
     public Friend addfriend(FriendRequestDto friendRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-
-        User friend = userRepository.findByUsername(friendRequestDto.getFriendname())
-                .orElseThrow(() -> new IllegalArgumentException("아이디를 찾을수 없습니다"));
+        //유저에 없는 사람을 추가 못하도록 함
+        User friend = userRepository.findUserByUsername(friendRequestDto.getFriendname())
+                .orElseThrow(() -> new IllegalArgumentException("유저를 찾을수 없습니다"));
 
         String nickname = friend.getNickname();
 
-        List<Friend> friends = friend.getFriends();
+        //자기 자신을 추가 못하게 함
+        User userTemp = userRepository.findById(userDetails.getUser().getId())
+                .orElseThrow();
+        if (userTemp.getUsername().equals(friendRequestDto.getFriendname()))
+            throw new CustomException(FRIEND_ADD_CODE);
+
+        //이미 등록된 친구를 친구추가 할 수 없도록 함
+        List<Friend> friends =friendRepository.findAll();
         for(Friend overlapUser : friends) {
             if (overlapUser.getFriendname().equals(friendRequestDto.getFriendname()))
                 throw new CustomException(FRIEND_CHECK_CODE);
