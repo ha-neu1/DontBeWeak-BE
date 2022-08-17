12a5073b5cfaa89bf77a027ae14a56979cb93b09
ju0song <cuobalee@naver.com>
2022-08-17 11:46:49 +0900

    [friend] -유저 리스트에 로그인 한 사람의 친구만 나오게

--- src/main/java/com/finalproject/dontbeweak/controller/FriendController.java
+++ src/main/java/com/finalproject/dontbeweak/controller/FriendController.java
@@ -34,8 +34,8 @@ public class FriendController {
 
     //친구목록 조회
     @GetMapping("/friend")
-    public  ResponseEntity<List<FriendResponseDto>> listfriend(){
-        List<FriendResponseDto> friendResponseDtoList = friendService.listfriend();
+    public  ResponseEntity<List<FriendResponseDto>> listfriend(@AuthenticationPrincipal UserDetailsImpl userDetails){
+        List<FriendResponseDto> friendResponseDtoList = friendService.listfriend(userDetails);
         return ResponseEntity.status(HttpStatus.OK)
                 .body(friendResponseDtoList);
     }
