bd8bc2b1694c493a5816a17c7e6d07013d2ad129
HRYUN <yunhyerim41@gmail.com>
2022-08-21 00:57:04 +0900

    [CAT] - Pet interface 생성 후 Cat에 적용. - 고양이 이미지 없어서 생성 안될 경우의 에러코드 추가

--- src/main/java/com/finalproject/dontbeweak/repository/CatImageRepository.java
+++ src/main/java/com/finalproject/dontbeweak/repository/CatImageRepository.java
@@ -3,7 +3,8 @@ package com.finalproject.dontbeweak.repository;
 import com.finalproject.dontbeweak.model.CatImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CatImageRepository extends JpaRepository<CatImage, Long> {
-    CatImage findCatImageByChangeLevel(int level);
+import java.util.Optional;
 
+public interface CatImageRepository extends JpaRepository<CatImage, Long> {
+    Optional<CatImage> findCatImageByChangeLevel(int level);
 }
