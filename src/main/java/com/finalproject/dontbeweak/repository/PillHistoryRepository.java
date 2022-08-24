a93396294be2fced85c9263fe7849efcfebf304d
ha-neu1 <107821879+ha-neu1@users.noreply.github.com>
2022-08-24 15:18:27 +0900

    [PILLHISTORY] - 테이블 다시 생성(이전의 donepill)

---
+++ src/main/java/com/finalproject/dontbeweak/repository/PillHistoryRepository.java
@@ -0,0 +1,6 @@
+package com.finalproject.dontbeweak.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PillHistoryRepository extends JpaRepository<PillRepository, Long> {
+}
