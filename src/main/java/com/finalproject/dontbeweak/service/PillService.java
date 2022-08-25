ee23310e19b525a065870653034df349efbaf05a
ha-neu1 <107821879+ha-neu1@users.noreply.github.com>
2022-08-26 02:39:27 +0900

    전체적인 코드 및 주석 정리

--- src/main/java/com/finalproject/dontbeweak/service/PillService.java
+++ src/main/java/com/finalproject/dontbeweak/service/PillService.java
@@ -37,13 +37,8 @@ public class PillService {
                 () -> new IllegalArgumentException("회원이 존재하지 않습니다.")
         );
 
-        System.out.println("영양제 생성 중");
         Pill pill = new Pill(user, pillRequestDto);
-        System.out.println("영양제 생성 완료");
-
-        System.out.println("영양제 등록 중");
         pillRepository.save(pill);
-        System.out.println("영양제 등록 완료");
 
         return new PillResponseDto(pill);
     }
@@ -62,28 +57,17 @@ public class PillService {
 
     //영양제 복용 완료
     public PillHistoryResponseDto donePill(PillHistoryRequestDto pillHistoryRequestDto, UserDetailsImpl userDetails) {
-
-        System.out.println("유저 찾는 중...");
         User user = userRepository.findByUsername(userDetails.getUsername()).orElseThrow(
                 () -> new IllegalArgumentException("회원이 존재하지 않습니다.")
         );
+
         String productName = pillHistoryRequestDto.getProductName();
 
-        System.out.println("영양제 찾는 중...");
         Pill pill = pillRepository.findByUser_IdAndProductName(user.getId(), productName);
-        System.out.println("영양제 찾기 완료");
-
-
         pill.donePill();
-        System.out.println("영양제 체크, 시간 입력 완료");
-
-
-        System.out.println("변경 저장하기");
         pillRepository.save(pill);
-        System.out.println("변경 저장 완료");
 
         PillHistory pillHistory = new PillHistory(user, pill, pillHistoryRequestDto);
-
         pillHistoryRepository.save(pillHistory);
 
         return new PillHistoryResponseDto(pill, pillHistory);
@@ -101,7 +85,6 @@ public class PillService {
     }
 
     //주간 영양제 복용 조회
-
     @Transactional
     public List<PillHistoryResponseDto> getPillList(String username, String startDate, String endDate) {
         Optional<User> user = userRepository.findByUsername(username);
