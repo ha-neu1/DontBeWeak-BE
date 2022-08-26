19ab311560418606c075dcfc1d176a1dbbac343f
ha-neu1 <107821879+ha-neu1@users.noreply.github.com>
2022-08-27 00:20:08 +0900

    [PILL] -주석 삭제

--- src/main/java/com/finalproject/dontbeweak/pill/repository/PillRepository.java
+++ src/main/java/com/finalproject/dontbeweak/pill/repository/PillRepository.java
@@ -8,6 +8,5 @@ import java.util.List;
 public interface PillRepository extends JpaRepository<Pill, Long> {
     List<Pill> findByUser_Username(String username);
     boolean existsByUser_IdAndProductName(Long userId, String productName);
-//    AndProductName
     Pill findByUser_IdAndProductName(Long userId, String productName);
 }
