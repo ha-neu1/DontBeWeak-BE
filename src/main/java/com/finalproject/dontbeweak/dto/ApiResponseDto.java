16e87ac77e5517de401056a767c8fe7bb909e664
ju0song <cuobalee@naver.com>
2022-09-02 20:54:20 +0900

    [api] -공공API 주소 수정 -상품명,제조일자를 DB에 넣을수있도록 수정

---
+++ src/main/java/com/finalproject/dontbeweak/dto/ApiResponseDto.java
@@ -0,0 +1,13 @@
+package com.finalproject.dontbeweak.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Builder
+public class ApiResponseDto {
+    private String PRDUCT;
+    private String DISTB_PD;
+}
