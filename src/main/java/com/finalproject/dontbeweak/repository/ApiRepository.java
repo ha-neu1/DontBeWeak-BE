0f85b93aa33a69dbeaed58ee8b34652a25ff1ee0
ju0song <cuobalee@naver.com>
2022-09-02 20:31:07 +0900

    [api] -api에 null값이 들어가있을떄 오류가나서 수정

---
+++ src/main/java/com/finalproject/dontbeweak/repository/ApiRepository.java
@@ -0,0 +1,7 @@
+package com.finalproject.dontbeweak.repository;
+
+import com.finalproject.dontbeweak.model.Api;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApiRepository extends JpaRepository<Api,Long> {
+}
