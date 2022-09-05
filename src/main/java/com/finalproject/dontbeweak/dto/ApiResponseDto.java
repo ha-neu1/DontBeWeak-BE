c15e5a678aaa4d5ac23ad05370b41dba7bdbc250
ha-neu1 <107821879+ha-neu1@users.noreply.github.com>
2022-09-05 15:08:19 +0900

    [API] -NULL 값 오류 해결 중

--- src/main/java/com/finalproject/dontbeweak/dto/ApiResponseDto.java
+++ src/main/java/com/finalproject/dontbeweak/dto/ApiResponseDto.java
@@ -1,12 +1,11 @@
 package com.finalproject.dontbeweak.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
-
 @NoArgsConstructor
 @AllArgsConstructor
-@Setter
 @Getter
-@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponseDto {
     private String PRDUCT;
     private String SRV_USE;
