e95aae27c5f21e2319317a186ca06c1ffd030769
HRYUN <yunhyerim41@gmail.com>
2022-09-14 08:19:06 +0900

    - 백엔드 ip 주소 변경

--- src/main/java/com/finalproject/dontbeweak/config/WebSecurityConfig.java
+++ src/main/java/com/finalproject/dontbeweak/config/WebSecurityConfig.java
@@ -106,7 +106,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
         configuration.addAllowedOrigin("http://localhost:3001");
         configuration.addAllowedOrigin("http://dontbeweak.s3-website.ap-northeast-2.amazonaws.com/");
         configuration.addAllowedOrigin("http://dontbeweak.kr/");
-        configuration.addAllowedOrigin("http://3.37.88.75/");
+        configuration.addAllowedOrigin("http://52.78.29.70/");
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
         configuration.addExposedHeader("Authorization");
