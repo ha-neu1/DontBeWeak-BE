4cc284d31c6d72daf9327b9f70d4b458a0e84ec3
배소정 <thfakdma2015@naver.com>
2022-09-13 16:25:54 +0900

    젠킨스 테스트

--- src/main/java/com/finalproject/dontbeweak/model/User.java
+++ src/main/java/com/finalproject/dontbeweak/model/User.java
@@ -42,7 +42,6 @@ public class User extends BaseEntity {
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "user", cascade = CascadeType.ALL)
     private List<Cat> cat;
 
-
     @OneToMany(mappedBy = "user",fetch = FetchType.LAZY,cascade = CascadeType.ALL)//mappedBy 연관관계의 주인이 아니다(나는 FK가 아니에요) DB에 컬럼 만들지 마세요.
     private List<Friend> friends = new ArrayList<>();
 
